78b71c62b7c1ac94e532cbea0f71ef55c52d7d42
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2015-05-15 13:05:43 +0100

    fixed triggered tests w/o --with-tests

--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,6 @@ function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function makeStateString(str) {
-	return str.toLowerCase().replace(/[^a-z0-9]+/g,'-');
-}
-
 function clone (src) {
 	return JSON.parse(JSON.stringify(src));
 }
